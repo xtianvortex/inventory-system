9c8ee987ee8fc440cdf61996e3f30a8584127f78
Eli Edrian Tan <eliedrian1996@gmail.com>
2014-11-29 17:29:18 +0800

    added static factory for models

--- src/main/AddItem.java
+++ src/main/AddItem.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package base;
+package main;
 
 /**
  *
  * @author tan
  */
-public abstract class PlaceholderClass {
+public class AddItem {
     
 }
---
+++ src/models/factory/ModelFactory.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models.factory;
+
+import models.Address;
+import models.Category;
+import models.Item;
+import models.Monkey;
+import models.Supplier;
+
+/**
+ *
+ * @author tan
+ */
+public class ModelFactory {
+    
+    /**
+     * 
+     * @return a new Item object
+     */
+    public static Item createItem(){
+        return new Item();
+    }
+    
+    /**
+     * 
+     * @return a new Category object
+     */
+    public static Category createCategory(){
+        return new Category();
+    }
+    
+    /**
+     * 
+     * @return a new Address object
+     */
+    public static Address createAddress(){
+        return new Address();
+    }
+    
+    /**
+     * 
+     * @return a new Monkey(User) object
+     */
+    public static Monkey createMonkey(){
+        return new Monkey();
+    }
+    
+    /**
+     * 
+     * @return a new Supplier object
+     */
+    public static Supplier createSupplier(){
+        return new Supplier();
+    }
+}
