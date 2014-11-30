8e1f77c0e5ee7a15d8040cd2e9dd2c421a7fcf6e
Eli Edrian Tan <eliedrian1996@gmail.com>
2014-11-30 16:01:47 +0800

    added additional models

---
+++ src/models/Department.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author tan
+ */
+
+@Entity
+@Table(name="DEPARTMENT")
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name="NAME")
+    private String name;
+    
+    // TODO
+}
---
+++ src/models/PurchaseRequest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author tan
+ */
+
+@Entity
+@Table(name="PURCHASE_REQUEST")
+public class PurchaseRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
+    @ManyToOne
+    private Department requestingDepartment;
+    
+    @ManyToOne
+    private Item requestedItem;
+    
+    @Column(name="REQUESTED_QUANTITY")
+    private int requestedQuantity;
+    
+    // TODO
+}
