36efc835ec3513c81893ee33743a51565785a2b0
Ton Saludadez <rontontoooon@gmail.com>
2014-11-30 17:57:56 +0800

    • Added Contact Column in Supplier Class • Encapsulated stuffs

--- src/models/Supplier.java
+++ src/models/Supplier.java
@@ -30,5 +30,48 @@ public class Supplier implements Serializable {
     @Embedded
     private Address address;
     
-    // TODO
+    @Embedded
+    private Contact contact;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the address
+     */
+    public Address getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the contact
+     */
+    public Contact getContact() {
+        return contact;
+    }
+
+    /**
+     * @param contact the contact to set
+     */
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
 }
