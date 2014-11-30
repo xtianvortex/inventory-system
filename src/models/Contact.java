821da4c3921f9526ba642d3163e4c7c960ba22ef
Ton Saludadez <rontontoooon@gmail.com>
2014-11-30 17:56:56 +0800

    • Created Contact Class • Added Contact Column in Supplier Class • Encapsulated stuffs

---
+++ src/models/Contact.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author lenovo
+ */
+
+@Embeddable
+public class Contact implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Column (name="CONTACT_NUMBER")
+    private String number;
+    
+    @Column (name="CONTACT_EMAIL")
+    private String email;
+
+    /**
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
